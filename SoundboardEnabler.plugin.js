985018f2d88483b2fa1f60e0974f68cdf1f59624
mattswagyt <128666675+mattswagyt@users.noreply.github.com>
2023-08-02 20:39:36 -0500

    Add files via upload

--- SoundboardEnabler.plugin.js
+++ SoundboardEnabler.plugin.js
@@ -1,39 +1,78 @@
-//META{"name":"SoundboardEnabler","version":"1.9.3","description":"Enables Discord's special soundboard feature without Nitro.","author":"BMF","source":"https://github.com/yourusername/soundboard-enabler-plugin"}*//
+//META{"name":"SoundboardEnabler","version":"1.9.3","description":"Enables Discord's special soundboard feature without Nitro.","author":"BMF","source":"https://github.com/mattswagyt/SoundboardEnabler"}*//
 
 console.log("SoundboardEnabler plugin started.");
 
 class SoundboardEnabler {
+  constructor() {
+    this.soundboardEnabled = false;
+    this.voiceChannel = null;
+    this.soundboardButton = null;
+
+    this.initialize();
+  }
+
+  initialize() {
+    // Listen for voice state updates
+    document.addEventListener("voiceStateUpdate", this.handleVoiceStateUpdate);
+
+    // Find the soundboard button and store a reference to it
+    this.soundboardButton = document.querySelector(".soundboardButton-2aSHcU");
+
+    // Add a click event listener to the soundboard button to unlock custom sounds
+    if (this.soundboardButton) {
+      this.soundboardButton.addEventListener("click", this.handleSoundboardButtonClick);
+    }
+  }
+
   start() {
-    // Enable the soundboard feature
-    this.enableSoundboard();
+    this.handleVoiceStateUpdate();
   }
 
   stop() {
-    // Disable the soundboard feature
     this.disableSoundboard();
+    this.soundboardButton.removeEventListener("click", this.handleSoundboardButtonClick);
   }
 
-  enableSoundboard() {
-    // Find the soundboard button and trigger a click event to enable it
-    const soundboardButton = document.querySelector(".soundboardButton-2aSHcU");
-    if (soundboardButton) {
-      soundboardButton.click();
+  handleVoiceStateUpdate = () => {
+    // Get the user's current voice channel
+    const userVoiceState = BdApi.findModuleByProps("getVoiceState").getVoiceState();
+    if (userVoiceState && userVoiceState.channelId) {
+      this.voiceChannel = userVoiceState.channelId;
+
+      // Enable the soundboard button
+      this.enableSoundboard();
     } else {
-      console.warn("Soundboard button not found.");
+      this.voiceChannel = null;
+
+      // Disable the soundboard button
+      this.disableSoundboard();
+    }
+  };
+
+  enableSoundboard() {
+    if (this.soundboardButton) {
+      this.soundboardButton.style.display = "block";
+      this.soundboardEnabled = true;
     }
   }
 
   disableSoundboard() {
-    // Find the soundboard button and trigger a click event to disable it
-    const soundboardButton = document.querySelector(".soundboardButton-2aSHcU.active-2W0W7Y");
-    if (soundboardButton) {
-      soundboardButton.click();
-    } else {
-      console.warn("Active soundboard button not found.");
+    if (this.soundboardButton) {
+      this.soundboardButton.style.display = "none";
+      this.soundboardEnabled = false;
     }
+  }
+
+  handleSoundboardButtonClick = () => {
+    // Logic for handling custom sounds on your server
+    // Replace this with your custom sound logic
+    this.playCustomSound();
+  };
+
+  playCustomSound() {
+    // Play your custom sound logic here
+    console.log("Playing custom sound...");
   }
 }
 
 const soundboardEnabler = new SoundboardEnabler();
-export default soundboardEnabler;
-
